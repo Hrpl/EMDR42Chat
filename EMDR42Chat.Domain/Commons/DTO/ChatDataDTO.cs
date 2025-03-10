380f3f7ebfd56ea6f5629df2c92f3a4597b80f9d
Hrpl <ponomarevd61@gmail.com>
2025-03-10 22:00:49 +0300

    edit model

--- EMDR42Chat.Domain/Commons/DTO/ChatDataDTO.cs
+++ EMDR42Chat.Domain/Commons/DTO/ChatDataDTO.cs
@@ -8,21 +8,11 @@ namespace EMDR42Chat.Domain.Commons.DTO;
 
 public class ChatDataDTO
 {
-    public bool IsShowInterface { get; set; }
-    public int ActiveTab { get; set; }
     public int SpeedObject { get; set; } = 30;
-    public int SecondsTimer { get; set; }
-    public int RepeatTimer { get; set; }
     public bool SwitchStartObject { get; set; } = false;
     public int QualObjects { get; set; } = 1;
     public int HeightObject { get; set; } = 40;
     public int SizeObject { get; set; } = 35;
-    public int TypeSound { get; set; }
-    public int Volume { get; set; }
-    public int SoundMode { get; set; }
-    public int Effect { get; set; }
-    public int Repeat { get; set; }
-    public int View { get; set; }
     public int TypeAnimation { get; set; } = 1;
     public int Figure { get; set; } = 1;
 
