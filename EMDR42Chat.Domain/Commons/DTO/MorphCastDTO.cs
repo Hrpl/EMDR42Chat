a9c6adce80e36bdd5468c4ac33224cc2c59f6948
Hrpl <ponomarevd61@gmail.com>
2025-04-27 01:22:27 +0300

    add data in model

--- EMDR42Chat.Domain/Commons/DTO/MorphCastDTO.cs
+++ EMDR42Chat.Domain/Commons/DTO/MorphCastDTO.cs
@@ -8,4 +8,15 @@ namespace EMDR42Chat.Domain.Commons.DTO;
 
 public class MorphCastDTO
 {
+    public int Arousal {  get; set; }
+    public int Valence { get; set; }
+    public int Attention { get; set; }
+    public int Angry { get; set; }
+    public int Disgust { get; set; }
+    public int Fear { get; set; }
+    public int Happy { get; set; }
+    public int Sad { get; set; }
+    public int Surprise { get; set; }
+    public int Neutral { get; set; }
+    public int AgeValue { get; set; }
 }
